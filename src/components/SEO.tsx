77684f970ce24f92f1032f1d6832f87a4c18bc13
Nader-Kojok <naderkojok@gmail.com>
2025-10-20 05:29:38 +0000

    chore: migrate to @vuer-ai/react-helmet-async from react-helmet-async

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet-async'
+import { Helmet } from '@vuer-ai/react-helmet-async'
 
 interface SEOProps {
   title: string
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
+import { HelmetProvider } from '@vuer-ai/react-helmet-async'
 import './index.css'
 import App from './App.tsx'
 import CGV from './pages/CGV.tsx'
